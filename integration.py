0697810fe30b9776f82139d469db125ec00802c4
劉哲豪 <jhowliu@gmail.com>
2018-07-24 17:21:13 +0800

    add integration

---
+++ integration.py
@@ -0,0 +1,17 @@
+def anonymous(x):
+    return x**2 + 1
+
+def integrate(func, start, end):
+    # step越小越接近
+    step = 0.1
+    intercept = start
+    area = 0
+
+    while intercept < end:
+        intercept += step
+        area += step*anonymous(intercept)
+    
+    return area
+
+if __name__ == '__main__':
+    print(integrate(anonymous, -10, 10))
