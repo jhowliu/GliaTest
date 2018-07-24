c63e5856799077685bd836def87e049d72dbd602
劉哲豪 <jhowliu@gmail.com>
2018-07-24 17:00:11 +0800

    add requirements

--- pttCrawler.py
+++ pttCrawler.py
@@ -4,6 +4,7 @@ import sys
 
 def start(board):
     c = PttWebCrawler(as_lib=True) 
+    # 目前抓最新的前五頁
     last_page = c.getLastPage(board)
     start_page = last_page-5 if last_page > 5 else 0
     c.parse_articles(start_page, last_page, board)
@@ -11,6 +12,7 @@ def start(board):
 if __name__ == '__main__':
 
     if len(sys.argv) < 2:
+        # python pttCrawler.py Soft_Job
         print('Usage: python pttCrawler.py <BoardName>')
         sys.exit(1)
 
